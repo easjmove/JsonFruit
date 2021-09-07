d14e784e203b2ca61b61f22f1237dc98fa5f11f8
zealand <zealand@ZEA-3603>
2021-09-07 15:17:47 +0200

    Add project files.

---
+++ JsonFruit/Fruit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace JsonFruit
+{
+    public class Fruit
+    {
+        public string typeOfFruit { get; set; }
+
+        public Fruit()
+        {
+        }
+
+        public Fruit(string typeOfFruit)
+        {
+            this.typeOfFruit = typeOfFruit;
+        }
+    }
+}
